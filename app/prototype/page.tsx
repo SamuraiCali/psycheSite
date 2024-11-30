ff1facfc35418c6617b3e803d96f4e5c28d6df56
SamuraiCali <xavierw.work@gmail.com>
2024-11-30 14:03:22 -0500

    2.3.2

--- app/prototype/page.tsx
+++ app/prototype/page.tsx
@@ -25,7 +25,7 @@ export default function HomePage() {
                 <CardContent>
                   <p className="mb-4 text-blue-600">{assessment.description}</p>
                   <Button asChild>
-                    <Link href={`@/app/prototype/assessments/leadership`}>
+                    <Link href={`/prototype/assessments/leadership`}>
                       Take Assessment
                     </Link>
                   </Button>
